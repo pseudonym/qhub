cc53e51fa56c04ab345890ec95e0fe95d4ebcecd
John Backstrand <sandos>
2004-07-07 08:39:44 +0000

    Compatibility with GCC ext/hash_map

---
+++ compat_hash_map.h
@@ -0,0 +1,51 @@
+#ifndef __COMPAT_HASH_
+#define __COMPAT_HASH_
+
+#include <string>
+
+#include "config.h"
+
+#ifdef HAVE_STD__HASH_MAP
+#include <hash_map>
+#endif
+
+#ifdef HAVE___GNU_CXX__HASH_MAP
+#include <hash_map>
+namespace std {
+  using __gnu_cxx::hash_map;
+};
+#endif
+
+#ifdef HAVE_STD__EXT_HASH_MAP
+#include <ext/hash_map>
+#endif
+
+#ifdef HAVE___GNU_CXX__EXT_HASH_MAP
+#include <ext/hash_map>
+namespace std {
+  using __gnu_cxx::hash_map;
+};
+#endif
+
+
+/** BEGIN FIX **/
+#if defined(HAVE___GNU_CXX__EXT_HASH_MAP) || defined(HAVE___GNU_CXX__HASH_MAP)
+namespace __gnu_cxx                                                                                 
+#else
+namespace std                                                                                 
+#endif
+{                                                                                             
+  template<> struct hash< std::string >                                                       
+  {                                                                                           
+    size_t operator()( const std::string& x ) const                                           
+    {                                                                                         
+      return hash< const char* >()( x.c_str() );                                              
+    }                                                                                         
+  };                                                                                          
+}          
+/** END FIX **/
+
+
+#endif
+
+
