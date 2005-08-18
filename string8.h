585da33898ac77d519575c6a3c1d287d30f1e084
John Backstrand <sandos>
2005-08-18 12:19:57 +0000

    Merge -r200:201 of IO-Refactor branch (remove unneccesary files and GCC 4.0 fixes)

--- string8.h
+++ string8.h
@@ -15,6 +15,9 @@ template<> struct char_traits<u_int8_t> {
 	static char_type* move(char_type* __s1, char_type const* __s2, size_t __n) {
 		return static_cast<char_type*>(memmove(__s1, __s2, __n));
 	}
+	static void assign(char_type& c1, const char_type& c2) {
+		c1 = c2;
+	}
 };
 
 typedef basic_string<u_int8_t, char_traits<u_int8_t>, std::allocator<u_int8_t> > string8;
