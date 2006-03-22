a6bde7638c36aa9f5e90cf70116d6f02f2065081
Matt Pearson <mjp64@cornell.edu>
2006-03-22 05:34:38 +0000

    oops, forgot Singleton

---
+++ src/Singleton.h
@@ -0,0 +1,26 @@
+#ifndef QHUB_SINGLETON_H
+#define QHUB_SINGLETON_H
+
+#include <boost/noncopyable.hpp>
+
+namespace qhub {
+
+template<typename T>
+class Singleton : protected boost::noncopyable {
+public:
+	static T* instance() throw()
+	{
+		if(!inst)
+			inst = new T();
+		return inst;
+	}
+
+protected:
+	static T* inst;
+};
+
+template<typename T> T* Singleton<T>::inst = 0;
+
+} // namespace qhub
+
+#endif // QHUB_SINGLETON_H
